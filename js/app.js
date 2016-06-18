9932b6265a82ac5438ef8ca6e26204e6cee7127f
Donghun <kmraen@gmail.com>
2016-06-18 17:21:17 +0900

    버그 수정 - 중복된 번호 문제

--- js/app.js
+++ js/app.js
@@ -10,5 +10,5 @@ function generate()
   	}  
   }  
   	return numbers; 
-} // 배열 안에 number가 포함되어 있는지 확인 function contains(numbers, number) {  for(var i=0; i<numbers.length; i++) {    if(numbers[i] == number) return true;  }  return false;}
+} // ==배열 안에 number가 포함되어 있는지 확인 ==/
 
